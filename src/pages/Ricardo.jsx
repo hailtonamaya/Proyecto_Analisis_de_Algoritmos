acfdc0c442e1f6050e2e399d2f2f61f35cf5e9ae
hailtonamaya <hailtonamaya@unitec.edu>
2024-12-03 15:29:05 -0600

    Routes

---
+++ src/pages/Ricardo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Ricardo = () => {
+  return (
+    <div>
+      <h1>Hola, soy Ricardo</h1>
+      <p>Este es un componente de ejemplo.</p>
+    </div>
+  );
+};
+
+export default Ricardo;
---
+++ src/pages/Steve.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Steve = () => {
+  return (
+    <div>
+      <h1>Hola, soy Steve</h1>
+      <p>Este es un componente de ejemplo.</p>
+    </div>
+  );
+};
+
+export default Steve;
